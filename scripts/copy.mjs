242d14bf86cd7bb031bda4f671625efbd881dc73
Mike Jerred <mjerred.work@gmail.com>
2022-04-21 18:54:03 +0100

    prevent search bot indexing

--- scripts/copy.mjs
+++ scripts/copy.mjs
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import { resolve } from 'path';
 
 await fs.cp(
-  resolve('glint/angular/dist'),
+  resolve('../git-glint/angular/dist'),
   resolve('docs'),
   { recursive: true }
 );
